afbf1441a8c016d9bb908e24134c824cba33b0d0
junioradeola <junioradeola@gmail.com>
2022-10-18 15:25:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar: and a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_purchar('_');
+	_purchar('p');
+	_purchar('u');
+	_purchar('t');
+	_purchar('c');
+	_purchar('h');
+	_purchar('a');
+	_purchar('r');
+	_purchar('\n');
+
+	return (0);
+}
